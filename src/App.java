87fd73f125d8bffe2a912b87ca5cc5b090cb9b66
Tommaso Garofolin <tommaso.garofolin99@gmail.com>
2021-10-24 11:11:44 +0200

    input JPanel component

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import frames.SimulatorFrame;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SimulatorFrame f = new SimulatorFrame();
+
+        f.setHP();
     }
 }
---
+++ src/frames/SimulatorFrame.java
@@ -0,0 +1,44 @@
+package frames;
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+public class SimulatorFrame extends JFrame {
+    public SimulatorFrame(){
+        super("Queue System Simulator");
+    }
+
+    public void setHP(){
+        this.setSize(800,800);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.setLayout(new GridLayout(1, 1));
+
+        this.add(this.setInputParams());
+
+        this.setVisible(true);
+    }
+
+    private JPanel setInputParams(){
+        JPanel inputParamsPanel = new JPanel();
+        FlowLayout panelLayout = new FlowLayout();
+
+        inputParamsPanel.setLayout(panelLayout);
+
+        inputParamsPanel.add(this.inputNumberPanel("Arrivals Occur", 0));
+        inputParamsPanel.add(this.inputNumberPanel("Service times", 0));
+        inputParamsPanel.add(this.inputNumberPanel("Packages number", 0));
+
+        return inputParamsPanel;
+    }
+
+    private JPanel inputNumberPanel(String inputLabel, float placeholder){
+        JPanel inputNumberPanel = new JPanel();
+        inputNumberPanel.setLayout(new GridLayout(2, 1));
+
+        inputNumberPanel.add(new JLabel(inputLabel));
+        inputNumberPanel.add(new JTextField(Float.toString(placeholder)));
+
+        return inputNumberPanel;
+    }
+}
