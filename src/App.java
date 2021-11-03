76cec9e96d6f3e51d24ce5760b391ea0852d6e07
Tommaso Garofolin <tommaso.garofolin99@gmail.com>
2021-11-03 19:20:20 +0100

    aggiunto piechart di test, semplicemente per vedere se funzionasse la libreria

--- src/App.java
+++ src/App.java
@@ -9,23 +9,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         FrameSimulatore f = new FrameSimulatore();
 
-        //f.setHP();
-
-        //MM1:
-        /* int lambda = 8;
-        int mu = 2;
-        int k = 10;
-        MM1 s1 = new MM1(lambda, mu);
-        //Assegnato variabili
-
-        System.out.println("rho = " +s1.rho());
-        System.out.println("Probabilita che il sistema sia vuoto = " +s1.P0());
-        System.out.println("Probabilita 10 pkt in coda = " +s1.PK(k));
-        System.out.println("Probabilita no coda = " +s1.noCoda());
-        System.out.println("Numero medio di pkt nel sistema= " +s1.LS());
-        System.out.println("Numero medio di pkt in coda= " +s1.LQ());
-        System.out.println("Tempo medio in attesa nel sistema= " +s1.WS());
-        System.out.println("Tempo medio in coda= " +s1.WQ()); */
+        f.setHP();
 
         //MMC:
         int lambda = 10;
--- src/frame/FrameSimulatore.java
+++ src/frame/FrameSimulatore.java
@@ -3,6 +3,12 @@ import javax.swing.*;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+
 public class FrameSimulatore extends JFrame {
     public FrameSimulatore(){
         super("Queue System Simulator");
@@ -15,6 +21,8 @@ public class FrameSimulatore extends JFrame {
         this.setLayout(new GridLayout(1, 1));
 
         this.add(this.setInputParams());
+
+        this.add(this.createChart());
 
         this.setVisible(true);
     }
@@ -40,5 +48,23 @@ public class FrameSimulatore extends JFrame {
         inputNumberPanel.add(new JTextField(Float.toString(placeholder)));
 
         return inputNumberPanel;
+    }
+
+    private JPanel createChart(){
+        DefaultPieDataset dataSet = new DefaultPieDataset();
+        dataSet.setValue("Chrome", 29);
+        dataSet.setValue("InternetExplorer", 36);
+        dataSet.setValue("Firefox", 35);
+       
+        // based on the dataset we create the chart
+        JFreeChart pieChart = ChartFactory.createPieChart3D("test", dataSet, true, true, false);
+        PiePlot plot = (PiePlot) pieChart.getPlot();
+        plot.setStartAngle(290);
+        plot.setForegroundAlpha(0.5f);
+
+        // Adding chart into a chart panel
+        ChartPanel chartPanel = new ChartPanel(pieChart);
+
+        return chartPanel;
     }
 }
